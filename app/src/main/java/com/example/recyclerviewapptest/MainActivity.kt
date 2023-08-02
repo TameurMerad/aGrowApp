d0e7bcd8e0aafdf8c6f7f206753035d0d83d59d2
Soufyan <tameur.merad@gmail.com>
2023-08-02 07:08:42 +0100

    coordinatorLayout problem fixed

--- app/src/main/java/com/example/recyclerviewapptest/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapptest/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.recyclerviewapptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.*
 
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(homeFrag())
     }
+
+    private fun replaceFragment (fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.flFrsgment , fragment).commit()
+    }
+
+
 }
---
+++ app/src/main/java/com/example/recyclerviewapptest/homeFrag.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerviewapptest
+
+import androidx.fragment.app.Fragment
+
+
+class homeFrag : Fragment (R.layout.fragment_home){
+
+
+    }
