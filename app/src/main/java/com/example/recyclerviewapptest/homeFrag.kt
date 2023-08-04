dfd0a289cb1f5e94f783ae4588de60793d64dc80
Soufyan <tameur.merad@gmail.com>
2023-08-04 15:04:02 +0100

    added pop ups

--- app/src/main/java/com/example/recyclerviewapptest/homeFrag.kt
+++ app/src/main/java/com/example/recyclerviewapptest/homeFrag.kt
@@ -1,6 +1,7 @@
 package com.example.recyclerviewapptest
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.WindowDecorActionBar
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
@@ -23,9 +25,12 @@ class homeFrag : Fragment () {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_home, container, false)
+        val showPopUp1 = popUpFrag()
+        val showPopUp2 = popUpFragTwo()
 
 
-requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
            view.findViewById<TextView>(R.id.followBtn).setOnClickListener(){
                view.findViewById<TextView>(R.id.followBtn).text = "Followed"
@@ -40,17 +45,33 @@ requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGH
             view.findViewById<android.widget.TextView>(com.example.recyclerviewapptest.R.id.followBtn4).text = "Followed"
         }
 
+
+
+
+
+
+
+
+
         view.findViewById<TextView>(R.id.helpBtn1).setOnClickListener(){
-            Toast.makeText(activity, "the first Contact Clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(activity, "the first Contact Clicked", Toast.LENGTH_SHORT).show()
+            showPopUp1.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+
         }
         view.findViewById<TextView>(R.id.helpBtn2).setOnClickListener(){
-            Toast.makeText(activity, "the second Contact Clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(activity, "the second Contact Clicked", Toast.LENGTH_SHORT).show()
+            showPopUp2.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+
         }
         view.findViewById<TextView>(R.id.helpBtn3).setOnClickListener(){
-            Toast.makeText(activity, "the third Contact Clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(activity, "the third Contact Clicked", Toast.LENGTH_SHORT).show()
+            showPopUp1.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+
         }
         view.findViewById<TextView>(R.id.helpBtn4).setOnClickListener(){
-            Toast.makeText(activity, "the fourth Contact Clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(activity, "the fourth Contact Clicked", Toast.LENGTH_SHORT).show()
+            showPopUp2.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+
         }
 
 
---
+++ app/src/main/java/com/example/recyclerviewapptest/popUpFrag.kt
@@ -0,0 +1,38 @@
+package com.example.recyclerviewapptest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+
+
+class popUpFrag : DialogFragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_pop_up, container, false)
+
+view.findViewById<Button>(R.id.cancelBtn).setOnClickListener(){
+    dismiss()
+}
+        view.findViewById<Button>(R.id.contributeBTN).setOnClickListener(){
+            dismiss()
+        }
+
+
+
+return view
+    }
+
+
+            }
